3c3f68fbb9b9263235e47a3b3b36abe6757b7601
wooinwoo <wooin1001@gmail.com>
2022-12-17 19:34:48 +0900

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1025px",
+      xl: "1280px",
+      xl2: "1360px",
+    },
+    extend: {
+      maxHeight: {},
+    },
+  },
+};
