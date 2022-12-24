faace316579c1ce985bdf31b55991434ca85dfd7
wooinwoo <wooin1001@gmail.com>
2022-12-24 12:35:40 +0900

    Add: colorPicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Canvas from "./components/canvas";
+import ColorPicker from "./components/colorPicker";
 
 function App() {
   return (
     <div className="pt-10 bg-gray-300 h-screen">
       <Canvas />
+      <ColorPicker />
     </div>
   );
 }
