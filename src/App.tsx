3ee34ab96a034dcb3373e1b099872154856de8af
wooinwoo <wooin1001@gmail.com>
2022-12-25 23:23:01 +0900

    Add: Components { lineThickness }

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Canvas from "./components/canvas";
 import ColorPicker from "./components/colorPicker";
+import LineThickness from "./components/lineThickness";
 
 function App() {
   return (
     <div className="pt-10 bg-gray-300 h-screen">
       <Canvas />
       <ColorPicker />
+      <LineThickness />
     </div>
   );
 }
---
+++ src/components/lineThickness.tsx
@@ -0,0 +1,3 @@
+export default function LineThickness() {
+  return <input type="range" name="points" min="0" max="10" />;
+}
