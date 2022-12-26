8e2a5063e008f6db0ac6bb9e7ff1d07800aee012
wooinwoo <wooin1001@gmail.com>
2022-12-26 23:30:51 +0900

    Add: RecoilRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
 );
